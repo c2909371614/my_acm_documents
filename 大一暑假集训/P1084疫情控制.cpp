fd901cf3570faba798390081965c57e664e3a135
c2909371614 <2909371614@qq.com>
2019-07-25 11:16:49 +0800

    倍增+lower_bound--洛谷p1084

--- 大一暑假集训/P1084疫情控制.cpp
+++ 大一暑假集训/P1084疫情控制.cpp
@@ -35,7 +35,7 @@ LL restmin[N];
 int checkok(int x, int las) { //5.dfs找未被“封死”的子树
 	int bj = 1, i, bbj = 0;
 	if(vis[x])return 1;//封死直接返回 1 
-	for(i=h[x]; i!=-1; i=ne[i]) {
+	for(i = h[x]; i != -1; i = ne[i]) {
 		if(to[i]==las)continue;
 		bbj=1;
 		if(!checkok(to[i],x)) {
@@ -47,44 +47,51 @@ int checkok(int x, int las) { //5.dfs找未被“封死”的子树
 	if(!bbj)return 0;
 	return bj;
 }
-bool cmp(node x,node y) {
-	return x.rest>y.rest;
+bool cmp(node x, node y) {
+	return x.rest > y.rest;
 }
 int check(LL lim) {
 	int i, j, x, now;
 	LL num;
 	na = nb = 0;
-	for(i = 1; i <= n; ++i)vis[i] = restbj[i] = 0;//初始化封死信息 
-	for(i = 1; i <= m; ++i)used[i] = 0;
+	for(i = 1; i <= n; ++i)
+		vis[i] = restbj[i] = 0;//初始化封死信息 
+	for(i = 1; i <= m; ++i)
+		used[i] = 0;
 	for(i = 1; i <= m; ++i) {
 		x = army[i], num = 0;
 		for(j = 17; j >= 0; --j) //3.上提军队
 			if(f[x][j] > 1 && num + dis[x][j] <= lim)
-				num+=dis[x][j],x=f[x][j];
-		if(f[x][0]==1&&num+dis[x][0]<=lim) { //4.处理剩余路程
-			a[++na].rest=lim-num-dis[x][0],a[na].id=i;
-			if(!restbj[x]||a[na].rest<restmin[x])
-				restmin[x]=a[na].rest,restbj[x]=i;
-		} else vis[x]=1;
+				num += dis[x][j],
+				x = f[x][j];
+		if(f[x][0] == 1 && num + dis[x][0] <= lim) { //4.处理剩余路程
+			a[++na].rest = lim - num - dis[x][0],
+			a[na].id = i;
+			if(!restbj[x] || a[na].rest < restmin[x])
+				restmin[x] = a[na].rest, 
+				restbj[x] = i;
+		} else vis[x] = 1;
 	}
-	if(checkok(1,0))return 1;
-	sort(a+1,a+1+na,cmp),sort(b+1,b+1+nb,cmp);//6.军队在子树间转移
-	now=1;
-	used[0]=1;
-	for(i=1; i<=nb; ++i) {
+	if(checkok(1, 0))
+		return 1;
+	sort(a+1, a+1+na, cmp);
+	sort(b+1, b+1+nb, cmp);//6.军队在子树间转移
+	now = 1;
+	used[0] = 1;
+	for(i = 1; i <= nb; ++i) {
 		if(!used[restbj[b[i].id]]) {
-			used[restbj[b[i].id]]=1;
+			used[restbj[b[i].id]] = 1;
 			continue;
 		}
-		while(now<=na&&(used[a[now].id]||a[now].rest<b[i].rest))++now;
-		if(now>na)return 0;
-		used[a[now].id]=1;
+		while(now <= na&&(used[a[now].id] || a[now].rest < b[i].rest))++now;
+		if(now > na)return 0;
+		used[a[now].id] = 1;
 	}
 	return 1;
 }
 int main() {
 	int i, x, y, z;
-	LL l = 0, r = 500000, mid, ans = -1;
+	LL l = 0, r = 5e5, mid, ans = -1;
 	cin >> n; 
 	for(i = 1; i <= n; ++i)h[i] = -1;//初始化h数组为-1 
 	for(i=1; i<n; ++i) {
@@ -92,13 +99,13 @@ int main() {
 		add(x,y,z), add(y,x,z);//加边 
 	}
 	dfs(1, 0, 0);//寻找未被封死的子树 
-	m = read();
+	cin >> m; 
 	for(i = 1; i <= m; ++i)army[i] = read();
-	while(l<=r) { //2.二分答案
-		mid=(l+r)>>1;
-		if(check(mid))r=mid-1,ans=mid;
-		else l=mid+1;
+	while(l <= r) { //2.二分答案
+		mid = (l + r) >> 1;
+		if(check(mid))r = mid - 1, ans = mid;
+		else l = mid + 1;
 	}
-	printf("%lld",ans);
+	printf("%lld", ans);
 	return 0;
 }
